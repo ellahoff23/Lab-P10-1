5c2263746ecbe94efb3b02a85720afc77344ecc6
Ella Hoffman <e-hoffman.2@onu.edu>
2024-04-11 08:10:07 -0400

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+string stringScramble(const string& word) {
+    if (word.length() <= 3) {
+
+        return word;
+    }
+
+    string scrambled = word; 
+    int pos1, pos2;
+
+    do {
+        pos1 = 1 + rand() % (word.length() - 2); 
+        pos2 = 1 + rand() % (word.length() - 2); 
+    } while (pos1 == pos2); 
+
+    char temp = scrambled[pos1];
+    scrambled[pos1] = scrambled[pos2];
+    scrambled[pos2] = temp;
+
+    return scrambled;
+}
+
+int main() {
+    srand(time(0)); 
+
+    string line;
+    cout << "Enter a line of text: ";
+    getline(cin, line);
+
+    vector<string> words;
+    size_t pos = 0;
+    while ((pos = line.find(' ', pos)) != string::npos) {
+        string word = line.substr(0, pos);
+        words.push_back(word);
+        line.erase(0, pos + 1); 
+    }
+    words.push_back(line);
+
+    for (const string& word : words) {
+        string scrambledWord = stringScramble(word);
+        cout << scrambledWord << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
